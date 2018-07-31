7b09f68ddb585ba9aab338a0af645817833d024b
Noelle Rolwes <nrolwes@gmail.com>
2018-07-31 15:48:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,6 +14,8 @@ function nowServing(katzDeliLine){
 }
 
 function currentLine(katzDeliLine){
+  var 
+  
   if (katzDeliLine.length>=1){
     return `The line is currently: 1. ${katzDeliLine[0]}, 2. ${katzDeliLine[1]}, 3. ${katzDeliLine[2]}`;
   } else {

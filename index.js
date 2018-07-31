5b4a582c25dc8c89e134b6eecef91a3cb9cba8c9
Noelle Rolwes <nrolwes@gmail.com>
2018-07-31 15:52:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function currentLine(katzDeliLine){
   var incrementNumbersWithNames = [];
   
   for (let katzDeliLineIndex = 0; katzDeliLineIndex <= katzDeliLine.length; katzDeliLineIndex++){
-    incrementNumbersWithNames.push `$
+    incrementNumbersWithNames.push `${katzDeliLineIndex
   }
   
   if (katzDeliLine.length>=1){
